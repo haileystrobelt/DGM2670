7a4b0990cd7d2ae066426353e8752d8c93f0ca2f
Hailey Strobelt <haileystrobelt@gmail.com>
2020-09-17 12:46:01 -0600

    New Scripts

---
+++ DGM2670/Assets/Scripts/Lerping.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Lerping : MonoBehaviour
+{
+    public Vector3 vOne, vTwo;
+
+    private float speed = 2f;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var direction = Vector3.Lerp(vOne, vTwo, 1);
+        transform.position = direction;
+    }
+}
---
+++ DGM2670/Assets/Scripts/MoveLocator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MoveLocator : MonoBehaviour
+{
+    private Camera cam;
+    public Transform pointObj;
+    
+    
+    private void Start()
+    {
+        cam = Camera.main
+    }
+
+    private void OnMouseDown()
+    {
+        if (Physics.Raycast(cam.ScreenPointToRay()))
+    }
+}
